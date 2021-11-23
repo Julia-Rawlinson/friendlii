2e2346990e84738c6dab454a7430459d27401bd5
Julia-Rawlinson <rawlinsonjulia@gmail.com>
2021-11-22 19:31:02 -0500

    Initial Commit

---
+++ JULIA_Friends/JULIA_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JULIA_FriendsApp.swift
+//  JULIA_Friends
+//
+//  Created by Julia Rawlinson on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct JULIA_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
